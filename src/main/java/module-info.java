448482fc316f97c2594d34af30d90af97b8853d4
TK3dv <franciscotortillol@gmail.com>
2021-12-17 18:00:21 +0100

    Inicio proyecto Colores by Francisco Tortillol and Carlos J. Sánchez,  en el directorio documentacion se encuentran los documentos Prototipo.pdf y vista_jerarquica.odt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.tk3dv.colores {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.tk3dv.colores to javafx.fxml;
+    exports org.tk3dv.colores;
+}
---
+++ src/main/java/org/tk3dv/colores/Controller.java
@@ -0,0 +1,7 @@
+package org.tk3dv.colores;
+
+
+
+public class Controller {
+
+}
