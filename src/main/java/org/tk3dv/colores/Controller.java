193d5fe2ed23b4bc4d4ee1e99a115bbd5d0f730c
TK3dv <franciscotortillol@gmail.com>
2021-12-18 13:42:02 +0100

    En la clase controller se asignan los valores de los sliders a los labels

--- src/main/java/org/tk3dv/colores/Controller.java
+++ src/main/java/org/tk3dv/colores/Controller.java
@@ -1,8 +1,45 @@
 package org.tk3dv.colores;
 
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class Controller implements Initializable {
 
+    @FXML
+    private Slider sliderAzul, sliderRojo, sliderVerde;
+
+    @FXML
+    private Label lbRojo, lbVerde, lbAzul;
+
+    int valor;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        sliderRojo.valueProperty().addListener((observableValue, number, t1) -> {
+            valor=(int)sliderRojo.getValue();
+            lbRojo.setText(Integer.toString(valor));
+
+        });
+
+        sliderVerde.valueProperty().addListener((observableValue, number, t1) -> {
+            valor=(int)sliderVerde.getValue();
+            lbVerde.setText(Integer.toString(valor));
+
+        });
+
+        sliderAzul.valueProperty().addListener((observableValue, number, t1) -> {
+            valor=(int)sliderAzul.getValue();
+            lbAzul.setText(Integer.toString(valor));
+
+        });
+
+    }
 }
