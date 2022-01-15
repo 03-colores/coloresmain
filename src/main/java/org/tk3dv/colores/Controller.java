1f7cabc6d971840ff0e1e48b09b74fdf5f626de0
TK3dv <franciscotortillol@gmail.com>
2022-01-15 13:04:46 +0100

    añadida forma

--- src/main/java/org/tk3dv/colores/Controller.java
+++ src/main/java/org/tk3dv/colores/Controller.java
@@ -1,13 +1,18 @@
 package org.tk3dv.colores;
 
 
+import eu.hansolo.tilesfx.colors.ColorSkin;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.css.converter.ColorConverter;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import org.tk3dv.colores.modelo.Colores;
 import javafx.event.ActionEvent;
 
@@ -26,29 +31,38 @@ public class Controller implements Initializable {
     @FXML
     private ListView<Colores> lvColores;
 
+    @FXML
+    private Rectangle shape;
+
     int valor;
     ObservableList<Colores> lista = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+//        sliderRojo.valueProperty().bind(shape.fillProperty().setValue());
+
+//        shape.fillProperty().getValue();
+
         sliderRojo.valueProperty().addListener((observableValue, number, t1) -> {
             valor=(int)sliderRojo.getValue();
             lbRojo.setText(Integer.toString(valor));
+          //  shape.setFill(Color.rgb(valor,0,0));
 
         });
 
         sliderVerde.valueProperty().addListener((observableValue, number, t1) -> {
             valor=(int)sliderVerde.getValue();
             lbVerde.setText(Integer.toString(valor));
-
+           // shape.setFill(Color.rgb(0,valor,0));
         });
 
         sliderAzul.valueProperty().addListener((observableValue, number, t1) -> {
             valor=(int)sliderAzul.getValue();
             lbAzul.setText(Integer.toString(valor));
-
+           // shape.setFill(Color.rgb(0,0,valor));
         });
+
 
 
 
