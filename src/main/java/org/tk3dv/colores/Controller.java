f39de6635ba2be1b5a29892f27263c7b1a141796
TK3dv <franciscotortillol@gmail.com>
2022-02-28 16:27:53 +0100

    Jerarquia de la vista modificada, modelo optimizado y añadidos los comentarios JavaDoc en el controlador

--- src/main/java/org/tk3dv/colores/Controller.java
+++ src/main/java/org/tk3dv/colores/Controller.java
@@ -16,6 +16,15 @@ import org.tk3dv.colores.modelo.Colores;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Clase controlador de la aplicacion según el
+ * {@see}<a href="https://developer.mozilla.org/es/docs/Glossary/MVC">
+ * patrón de diseño MVC.</a>
+ * Es la encargada de manejar la lógica que actualiza el modelo y la vista.
+ * @since 1.0
+ * @author Francisco Tortillol Molina & Carlos José Sanchez Carmona
+ * @see <a href="https://github.com/03-colores/coloresmain.git">Repositorio en GitHub</a>
+  */
 public class Controller implements Initializable {
 
     @FXML
@@ -29,11 +38,67 @@ public class Controller implements Initializable {
     @FXML
     private Button btnEliminar;
     private int valorRojo,valorAzul,valorVerde;
-    ObservableList<Colores>lista = FXCollections.observableArrayList();
+    private ObservableList<Colores>lista = FXCollections.observableArrayList();
 
+    /**
+     * Heredado de la interfaz Initializable.En el se inicializa el Listview y
+     * se establece el color de relleno inicial del Rectangle, ademas de declararse
+     * los métodos que se ejecutan al iniciarse y durante el tiempo de ejecución
+     * de la app
+     * @param url del la clase URL
+     * @param resourceBundle del la clase ResourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        rectangulo.setFill(Color.rgb(0,0,0));
+        lvColores.setItems(lista);
+        gestionarSlider();
+        customItemsListView();
+        buttonBinderToItem();
+    }
+
+    /**
+     * Enlaza la propiedad de la Clase Button que devuelve un valor boolean
+     * en función de si está activo (true) o no (false) el botón eliminar con
+     * la propiedad de cada item que forma parte del ListView devolviendo un
+     * valor boolean en función de si este está seleccionado (true) o no lo esta
+     * (false).
+     */
+    public void buttonBinderToItem(){
+        btnEliminar.disableProperty().bind(lvColores.getSelectionModel().selectedItemProperty().isNull());
+    }
+    /**
+     * Añade un objeto de la clase ChangeListener al valueProperty de cada Slider
+     * este será notificado cada vez que su valor cambie.
+     * Cuando esto suceda se establecera el texto mostrado por el Label
+     * correspondiente con el valor recibido por la propiedad valueProperty del
+     * slider.
+     */
+    public void gestionarSlider(){
+
+        sliderRojo.valueProperty().addListener((observableValue, number, t1) -> {
+            lbRojo.setText(Integer.toString(valorRojo= t1.intValue()));
+            rectangulo.setFill(Color.rgb(valorRojo, valorVerde, valorAzul));
+        });
+
+        sliderVerde.valueProperty().addListener((observableValue, number, t1) -> {
+            lbVerde.setText(Integer.toString(valorVerde= t1.intValue()));
+            rectangulo.setFill(Color.rgb(valorRojo, valorVerde, valorAzul));
+        });
+
+        sliderAzul.valueProperty().addListener((observableValue, number, t1) -> {
+            lbAzul.setText(Integer.toString(valorAzul= t1.intValue()));
+            rectangulo.setFill(Color.rgb(valorRojo, valorVerde, valorAzul));
+        });
+    }
+    /**
+     * Customiza el aspecto de cada uno de los items que conforman el ListView
+     * para ello hace uso de su método setCellFactory que permite personalizar
+     * la instancia de ListCell encargada de darles un aspecto por defecto.
+     * Mediante este método añade un objeto rectangle a cada item.
+     */
+    public void customItemsListView(){
         lvColores.setCellFactory(new Callback<ListView<Colores>, ListCell<Colores>>() {
             @Override
             public ListCell<Colores> call(ListView<Colores> param) {
@@ -54,44 +119,15 @@ public class Controller implements Initializable {
                 return mixto;
             }
         });
-
-        rectangulo.setFill(Color.rgb(0,0,0));
-        lvColores.setItems(lista);
-        /*
-        * Se añade un Listener a cada uno de los slider, que cambia el texto
-        * de la etiqueta correspondiente */
-
-        sliderRojo.valueProperty().addListener((observableValue, number, t1) -> {
-            lbRojo.setText(Integer.toString(valorRojo= t1.intValue()));
-            rectangulo.setFill(Color.rgb(valorRojo, valorVerde, valorAzul));
-        });
-
-        sliderVerde.valueProperty().addListener((observableValue, number, t1) -> {
-            lbVerde.setText(Integer.toString(valorVerde= t1.intValue()));
-            rectangulo.setFill(Color.rgb(valorRojo, valorVerde, valorAzul));
-        });
-
-        sliderAzul.valueProperty().addListener((observableValue, number, t1) -> {
-            lbAzul.setText(Integer.toString(valorAzul= t1.intValue()));
-            rectangulo.setFill(Color.rgb(valorRojo, valorVerde, valorAzul));
-        });
-        /*Enlaza la propiedad desactivar del boton 'Eliminar' con la propiedad de los
-        * item del ListView que comprueba que esta seleccionado para mantener el botón
-        * desactivado mientras no se seleccione un item del ListView */
-        btnEliminar.disableProperty().bind(lvColores.getSelectionModel().selectedItemProperty().isNull());
-
     }
-
-
+    /**
+     * Crea una instancia de la clase modelo y la añade a la ObservableList que
+     * compone los items del ListView.
+     * @param event Recibe la notificación de que el botón ha sido pulsado.
+     */
     @FXML
     void addValores(ActionEvent event) {
-        //obtencion de los valores de los slider para crear un colores y añadirlo a la lista y lvColores
         Colores colores = new Colores(
-                new Rectangle(30.00,10.00,
-                        Color.rgb(Integer.parseInt(lbRojo.getText()),
-                                  Integer.parseInt(lbVerde.getText()),
-                                  Integer.parseInt(lbAzul.getText()))
-                ),
                 Integer.parseInt(lbRojo.getText()),
                 Integer.parseInt(lbVerde.getText()),
                 Integer.parseInt(lbAzul.getText())
@@ -99,9 +135,13 @@ public class Controller implements Initializable {
         lista.add(colores);
     }
 
+    /**
+     * Elimina el objeto de la lista que corresponde al indice del item
+     * seleccionado.
+     * @param event Recibe la notificación de que el botón ha sido pulsado.
+     */
     @FXML
     void deleteValores(ActionEvent event) {
-            //obtener el index de la seleccion de 1vColores para eleminarlo de la lista
             lista.remove(lvColores.getSelectionModel().getSelectedIndex());
          }
     }
--- src/main/java/org/tk3dv/colores/modelo/Colores.java
+++ src/main/java/org/tk3dv/colores/modelo/Colores.java
@@ -1,37 +1,43 @@
 package org.tk3dv.colores.modelo;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+
 public class Colores {
-    /*Declaro las variable del tipo IntegerProperty que contendra cada uno de los
+
+    /*Declaro las variables del tipo Integer que contendrá cada uno de los
     * valores que en conjunto formaran cada item del ListView*/
-    private final IntegerProperty numR = new SimpleIntegerProperty( this, "numR",0);
-    private final IntegerProperty numV = new SimpleIntegerProperty( this, "numV",0);
-    private final IntegerProperty numA = new SimpleIntegerProperty( this, "numA",0);
+    private Integer numR;
+    private Integer numV;
+    private Integer numA;
     private Rectangle rectangle;
 
 
     public Colores() {}
 
-    public Colores(Rectangle rectangle,Integer numR, Integer numV, Integer numA) {
-        this.numR.set(numR);
-        this.numV.set(numV);
-        this.numA.set(numA);
-        this.rectangle = rectangle;
+
+    public Colores(Integer numR, Integer numV, Integer numA)
+    {
+        this.numR = numR;
+        this.numV=numV;
+        this.numA=numA;
+        this.rectangle = new Rectangle(30,10,Color.rgb(numR,numV,numA));
     }
-    public int getNumR() {return numR.get();}
-    public IntegerProperty numRProperty() {return numR;}
-    public void setNumR(int numR) {this.numR.set(numR);}
-    public int getNumV() {return numV.get();}
-    public IntegerProperty numVProperty() {return numV;}
-    public void setNumV(int numV) {this.numV.set(numV);}
-    public int getNumA() {return numA.get();}
-    public IntegerProperty numAProperty() {return numA;}
-    public void setNumA(int numA) {this.numA.set(numA);}
+
+
+    public int getNumR() {return numR;}
+    public int getNumV() {return numV;}
+    public int getNumA() {return numA;}
+
+    public void setNumR(int numR) {this.numR=numR;}
+    public void setNumV(int numV) {this.numV=numV;}
+    public void setNumA(int numA) {this.numA=numA;}
+
+
     public Rectangle getRectangle() {return rectangle;}
-    public void setRectangle(Rectangle rectangle) {this.rectangle = rectangle;}
+
     @Override
-    public String toString() {return "RGB "+numR.get() +" : "+ numV.get() +" : " + numA.get();}
+    public String toString() {return "RGB "+numR +" : "
+            + numV +" : " + numA;}
 }
