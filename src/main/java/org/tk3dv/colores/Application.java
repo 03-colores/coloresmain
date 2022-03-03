2bd7fec0e04674051638cb565c1fd09f0cea1faf
FranToMo <7787918@alu.murciaeduca.es>
2022-03-03 12:46:56 +0100

    Comentarios JavaDoc en la clase principal 'Application.java'

--- src/main/java/org/tk3dv/colores/Application.java
+++ src/main/java/org/tk3dv/colores/Application.java
@@ -6,10 +6,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
+/**
+ * Clase principal, utilizando JavaFX, crea un selector de colores utilizando
+ * sliders
+ * @since 1.0
+ * @author Francisco Tortillol Molina & Carlos José Sanchez Carmona
+ * @see <a href="https://github.com/03-colores/coloresmain.git">Repositorio en GitHub</a>
+ */
 public class Application extends javafx.application.Application {
 
-
+    /**
+     * Carga los parámetros de la vista definidos en el archivo fxml los
+     * cuales pasaran a ser uno de los parámetros de una instancia del objeto
+     * de la clase Scene, junto con sus dimensiones.
+     *
+     * @param stage Recibe una instancia de la clase Scene y se muestra.
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("ColoresView.fxml"));
@@ -20,7 +33,10 @@ public class Application extends javafx.application.Application {
         stage.show();
     }
 
-
+    /**
+     * Método principal de la aplicación
+     * @param args
+     */
     public static void main(String[] args) {
         launch();
     }
